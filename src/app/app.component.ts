2bb4975139571f78df02ce5e7faa274d2fa5beea
Narasegowda Hanumaiah <narasegowda.hanumaia@slkgroup.com>
2020-06-23 13:13:30 +0530

    Material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,28 @@ import { Component, OnInit } from '@angular/core';
 import { CountryService } from './shared/county.service';
 import { Country } from './shared/country';
 import { Observable, Subject } from 'rxjs';
-
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from "@angular/platform-browser";
 import {
   tap,
   switchMap,
   debounceTime,
   distinctUntilChanged
 } from "rxjs/operators";
+export interface VehicleData {
+  store: string;
+  year: number;
+  make: number;
+  model: number;
+  Appraisal: number;
+}
 
+const logoURL = 
+"../assets/images/EchoPark Logo@2x.png";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 
 export class AppComponent implements OnInit {
@@ -21,12 +31,22 @@ export class AppComponent implements OnInit {
   countries$: Observable<Country[]>;
   private searchTerms = new Subject<string>();
 
-  constructor(private countryService: CountryService) { }
+  constructor(private countryService: CountryService,private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer) {  this.matIconRegistry.addSvgIcon(
+      "logo",
+      this.domSanitizer.bypassSecurityTrustResourceUrl(logoURL));}
 
   search(term: string) {
     this.searchTerms.next(term);
   }
-
+  dataSource: VehicleData[] = [
+    {store: 'Yogurt', year: 159, make: 6, model: 24, Appraisal: 4},
+    {store: 'Sandwich', year: 237, make: 9, model: 37, Appraisal: 4},
+    {store: 'Eclairs', year: 262, make: 16, model: 24, Appraisal: 6},
+    {store: 'Cupcakes', year: 305, make: 4, model: 67, Appraisal: 4},
+    {store: 'Gingerbreads', year: 356, make: 16, model: 49, Appraisal: 4},
+ ];
+ displayedColumns: string[] = ['store', 'year', 'make', 'model','Appraisal'];
   ngOnInit(): void {
     this.countries$ = this.searchTerms.pipe(
       tap(_ => this.loading = true),
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,31 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CountryService } from './shared/county.service';
 import { HttpClientModule } from "@angular/common/http";
+import { MaterialModule } from './material/material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import{MatTableModule} from '@angular/material/table';
+import{MatSortModule} from '@angular/material/sort';
+import{MatPaginatorModule} from '@angular/material/paginator';
+import{MatInputModule} from '@angular/material/input';
+import{MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
-    HttpClientModule
+    MaterialModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatInputModule,
+    MatProgressSpinnerModule
   ],
   providers: [CountryService],
   bootstrap: [AppComponent]
